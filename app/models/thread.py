9988021c8906109455ad0bbaec6cfad0f8b01362
Mattéo Cuisset <matteo.cuisset@gmail.com>
2024-12-22 14:24:45 +0100

    Refactor Thread model and add UpdateThread model for improved thread management functionality.

--- app/models/thread.py
+++ app/models/thread.py
@@ -2,10 +2,26 @@ from pydantic import BaseModel, Field, UUID5
 from ..utils import generate_uuid
 
 
-class Thread(BaseModel):
+class ThreadModel(BaseModel):
+    """
+    A model for a thread.
+    
+    PS: It can be used as a a creation model for a thread.
+    """
     id: str | UUID5 = Field(default_factory=generate_uuid)
     name: str
-    public: bool
+    public: bool = False
     id_owner: str | UUID5
     moderators: list[str |UUID5] | None = None
     members: list[str | UUID5] | None = None
+    banned_users: list[str | UUID5] | None = None
+
+class UpdateThreadModel(BaseModel):
+    """
+    A model for updating a thread.
+    """
+    name: str | None = None
+    public: bool | None = None
+    moderators: list[str | UUID5] | None = None
+    members: list[str | UUID5] | None = None
+    banned_users: list[str | UUID5] | None = None
