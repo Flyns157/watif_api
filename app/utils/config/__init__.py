60104c3a9441c53821fd8820d5afc59adef25a81
Mattéo Cuisset <matteo.cuisset@gmail.com>
2024-12-23 21:02:14 +0100

    feat(config): add environment variables for MongoDB and admin settings

--- app/utils/config/__init__.py
+++ app/utils/config/__init__.py
@@ -11,6 +11,8 @@ load_dotenv()
 class Settings:
     MONGODB_URI: str = os.getenv("MONGODB_URI")
     MONGODB_DATABASE: str = os.getenv("MONGODB_DATABASE")
+    MONGODB_USER: str = os.getenv("MONGODB_USER")
+    MONGODB_PASSWORD: str = os.getenv("MONGODB_PASSWORD")
     
     JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY")
     JWT_ALGORITHM: str = os.getenv("JWT_ALGORITHM")
@@ -20,3 +22,6 @@ class Settings:
     STAGE: Stage = Stage(os.getenv("STAGE"))
 
     DOMAIN_NAME: str = os.getenv("DOMAIN_NAME")
+
+    ADMIN_EMAIL: str = os.getenv("ADMIN_EMAIL")
+    ADMIN_PASSWORD: str = os.getenv("ADMIN_PASSWORD")
