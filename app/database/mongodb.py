19e817691c89b147058ef7eea564505537a19cc9
Mattéo Cuisset <matteo.cuisset@gmail.com>
2024-12-23 12:22:53 +0100

    Add CRUD operations and methods to MongoDB handler; include Pydantic model integration.

--- app/database/mongodb.py
+++ app/database/mongodb.py
@@ -1,3 +1,4 @@
+from pydantic import BaseModel
 import motor.motor_asyncio
 
 from ..utils.config import Settings
@@ -37,6 +38,41 @@ async def initialize_database():
         role_collection.insert_one(
             Role(name="admin" , rights=["*", "create:all", "read:all", "update:all", "delete:all", ], inherits=["user"]).model_dump()
         )
+
+# TODO: Add more methods for CRUD operations on other collections (and redefine the security model)
+def get_collection(collection_name: str | BaseModel):
+    if issubclass(collection_name, BaseModel):
+        collection_name = collection_name.__name__.lower()
+    return db.get_collection(collection_name)
+
+class MongoModel:
+    collection_name: str | BaseModel
+
+    @classmethod
+    async def find_one(cls, query: dict):
+        collection = get_collection(cls.collection_name)
+        return await collection.find_one(query)
+
+    @classmethod
+    async def find(cls, query: dict):
+        collection = get_collection(cls.collection_name)
+        return await collection.find(query)
+
+    @classmethod
+    async def insert_one(cls, document: dict):
+        collection = get_collection(cls.collection_name)
+        return await collection.insert_one(document)
+
+    @classmethod
+    async def update_one(cls, query: dict, update: dict):
+        collection = get_collection(cls.collection_name)
+        return await collection.update_one(query, update)
+
+    @classmethod
+    async def delete_one(cls, query: dict):
+        collection = get_collection(cls.collection_name)
+        return await collection.delete_one(query)
+
 
 if __name__ == "__main__":
     import asyncio
