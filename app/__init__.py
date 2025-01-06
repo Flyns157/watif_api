251011fc5c23c90aaa28f42bcf855fa8e1193ac7
Mattéo Cuisset <matteo.cuisset@gmail.com>
2025-01-06 13:23:16 +0100

    feat(logging): Add async function to get child logger

--- app/__init__.py
+++ app/__init__.py
@@ -20,6 +20,9 @@ logging.basicConfig(level=logging.INFO,
 main_logger = logging.getLogger(__name__)
 
 
+async def get_logger(name: str) -> logging.Logger:
+    return main_logger.getChild(name)
+
 from .database.mongodb import MongoManager
 
 mongodb = MongoManager()
